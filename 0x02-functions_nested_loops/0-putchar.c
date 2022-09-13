c96bef2caa682497a9f0dd1e99dbe78c4d3b5789
abdulswamad ramadhan <abdulswamad.ramadhan01@gmail.com>
2022-09-13 02:24:14 -0700

    initial

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main-entry point
- * Return Always 0 (success)
+ * main - entry point
+ * Return: Always 0 (success)
  */
-int main (void)
+int main(void)
 {
 putchar('_');
 putchar('p');
