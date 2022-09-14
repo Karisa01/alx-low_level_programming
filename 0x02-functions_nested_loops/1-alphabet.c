743353c340b6fe240ffa45cd0dacdbe29ed7224f
abdulswamad ramadhan <abdulswamad.ramadhan01@gmail.com>
2022-09-14 00:11:26 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print alphabets in lowercase followed by new line
+ * Return: Always 0 (seccess)
+ */
+int main(void)
+{
+int n;
+for (n='a'; n<='z'; n++)
+{
+_putchar(n);
+}
+_putchar('\n');
+}
