af9776fb27bb6c6d398f55e323842673d5760e35
abdulswamad ramadhan <abdulswamad.ramadhan01@gmail.com>
2022-09-15 01:59:56 -0700

    debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,10 +3,10 @@
  * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
-void positive_or_negative(int i);
+int main(void)
 {
 int i;
-i = 0;
+i = 98;
 positive_or_negative(i);
 return (0);
 }
