f1a755ffe70439772be070ea31998d0d86971984
abdulswamad ramadhan <abdulswamad.ramadhan01@gmail.com>
2022-09-15 01:55:02 -0700

    debugging

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include < stdio.h >
+
+void positive or negative ( int i ) ;
+int largest number ( int , int , int ) ;
+void print remaining days ( int , int , int ) ;
+int convert_day ( int , int ) ;
+
+#endif / MAIN_H "/
