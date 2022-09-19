e0a8b156527425a22ba1c79190ad118f80d756a7
abdulswamad ramadhan <abdulswamad.ramadhan01@gmail.com>
2022-09-19 03:23:57 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+void swap_int(int *a, int *b);
+{
+int a, b;
+printf("Enter Value of a ");
+scanf("%d", &a);
+printf("\nEnter Value of b ");
+scanf("%d", &b);
+int temp = a;
+a = b;
+b = temp;
+printf("\nAfter Swapping: a = %d, b = %d", x, y);
+return 0;
+}
