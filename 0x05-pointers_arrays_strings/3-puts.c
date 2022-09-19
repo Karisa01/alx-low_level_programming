196a98fea116a34343de4eca2319957e3761408a
abdulswamad ramadhan <abdulswamad.ramadhan01@gmail.com>
2022-09-19 04:59:17 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,12 +4,15 @@
  * @str: string input
  * Return: string
  */
+
 void _puts(char *str)
 {
-int i;
-for (i = 0; str[i] != '\0'; i++)
-{
-_putchar(str[i]);
-}
-_putchar('\n');
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
 }
