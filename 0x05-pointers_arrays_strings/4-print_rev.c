1edec90fec02de2362b0b4082a19d19dd4801346
abdulswamad ramadhan <abdulswamad.ramadhan01@gmail.com>
2022-09-19 05:45:34 -0700

    0x05 pointers

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,18 +1,21 @@
 #include "main.h"
 /**
- * print_rev - this characters in reverse.
- * @s: this is a parameter to change.
+ * print_rev - a function that prints a string, in revers
+ * fcounter is to first count to end, n is to count back
+ * @s: str input
+ * Return: string in reverse
  */
 void print_rev(char *s)
+{	
+int fcounter = 0;
+int i, n;
+for (i = 0; s[i] != '\0'; i++)
 {
-int i = 0;
-while (s[i] != 0)
-i++;
-i--;
-while (i >= 0)
+fcounter++;
+}
+for (n = (fcounter - 1); n >= 0; n--)
 {
-_putchar(s[i]);
-i--;
+_putchar(s[n]);
 }
 _putchar('\n');
 }
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,1 +1,1 @@
-0x05-pointers
+ 	POINTERS,ARRAYS AND STRINGS
