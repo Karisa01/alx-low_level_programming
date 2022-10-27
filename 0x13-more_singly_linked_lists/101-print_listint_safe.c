807d763433f1da0f2994419ee9426de49b0e116a
abdulswamad ramadhan <abdulswamad.ramadhan01@gmail.com>
2022-10-27 05:19:28 -0700

    0x13-more_singly

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,30 +1,49 @@
+
+
 #include <stdlib.h>
+
 #include <stdio.h>
+
 #include "lists.h"
+
 /**
+
  * print_listint_safe - print list
+
  * @head: head node
+
  * Return: amount of nodes in list
+
  */
+
 size_t print_listint_safe(const listint_t *head)
+
 {
+
 	int size = 0, i;
 
 	const listint_t *tmp[100];
+
+
 
 	if (!head)
 
 		exit(98);
 
+
+
 	while (head)
 
 	{
+
 		for (i = 0; i < size; i++)
 
 		{
+
 			if (tmp[i] == head)
 
 			{
+
 				printf("-> [%p] %d\n", (void *)head, head->n);
 
 				return (size);
@@ -32,6 +51,7 @@ size_t print_listint_safe(const listint_t *head)
 			}
 
 		}
+
 		printf("[%p] %d\n", (void *)head, head->n);
 
 		tmp[size] = head;
@@ -39,7 +59,9 @@ size_t print_listint_safe(const listint_t *head)
 		size++;
 
 		head = head->next;
+
 	}
 
 	return (size);
+
 }
