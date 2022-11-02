4e9abe63092820520e020dcf04c7b735e69a28b8
abdulswamad ramadhan <abdulswamad.ramadhan01@gmail.com>
2022-11-02 03:55:17 -0700

    0x15-file_io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -22,40 +22,30 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 
 		return (0);
-
 	fd = open(filename, O_RDWR);
 
 	if (fd == -1)
 
 		return (0);
-
 	buffer = malloc(letters);
 
 	if (buffer == NULL)
-
 	{
 		close(fd);
-
 		return (0);
 	}
 	r = read(fd, buffer, letters);
-
 	close(fd);
-
 	if (r == -1)
 
 	{
 		free(buffer);
-
 		return (0);
 	}
 	w = write(STDOUT_FILENO, buffer, r);
-
 	free(buffer);
-
 	if (r != w)
 
 		return (0);
-
 	return (w);
 }
